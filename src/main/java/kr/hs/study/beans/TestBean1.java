bb774a7d950c2b1d7a86a6672fc917ad7e73c020
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-15 12:12:55 +0900

    메서드 내용수정

--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -3,11 +3,11 @@ package kr.hs.study.beans;
 public class TestBean1 {
     TestBean1 (){}
     public void init_method(){
-        System.out.println("init_method");
+        System.out.println("TestBean1의 init_method");
     }
 
     public void destroy_method(){
-        System.out.println("destroy_method");
+        System.out.println("TestBean1의 destroy_method");
     }
 
 }
