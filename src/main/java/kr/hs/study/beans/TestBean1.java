9da04f3c703166d886fbefff1078a151eada7218
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-15 12:15:35 +0900

    내용 보충

--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -1,7 +1,9 @@
 package kr.hs.study.beans;
 
 public class TestBean1 {
-    TestBean1 (){}
+    TestBean1 (){
+        System.out.println("생성자 출력");
+    }
     public void init_method(){
         System.out.println("TestBean1의 init_method");
     }
