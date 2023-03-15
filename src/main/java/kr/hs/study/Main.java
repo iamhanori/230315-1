7513066b7312523a15b8c2e6f9b1738a3781df93
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-15 12:07:09 +0900

    1.기본생성자 생성 2.메서드 생성 3.객체 생성 요청 4.객체 주소 출력

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,7 +1,15 @@
 package kr.hs.study;
 
+import kr.hs.study.beans.TestBean1;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+
+        TestBean1 bean = ctx.getBean("bean", TestBean1.class);
+        bean.init_method();
+        bean.destroy_method();
+
     }
 }
---
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -0,0 +1,13 @@
+package kr.hs.study.beans;
+
+public class TestBean1 {
+    TestBean1 (){}
+    public void init_method(){
+        System.out.println("init_method");
+    }
+
+    public void destroy_method(){
+        System.out.println("destroy_method");
+    }
+
+}
